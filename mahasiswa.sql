ad2788983f91140ded3904b32f120474ef9c07c9
babang0 <148828583+babang0@users.noreply.github.com>
2024-01-02 17:24:39 +0700

    Add files via upload

---
+++ mahasiswa.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 02, 2024 at 11:22 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpdasar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(250) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
+  `umur` varchar(250) NOT NULL,
+  `status` varchar(250) NOT NULL,
+  `Gmail` varchar(50) NOT NULL,
+  `posisi` varchar(50) NOT NULL,
+  `nohp` varchar(15) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `alamat`, `umur`, `status`, `Gmail`, `posisi`, `nohp`, `gambar`) VALUES
+(12, 'ari', 'sebapo', '19', 'menikah', 'marselynindia@gmail.com', 'karyawan', '082181694770', '659379a2b3583.jpg'),
+(13, 'nindia marselly', 'ibru', '19', 'mahasiswa', 'marselynindia@gmail.com', 'karyawan', '082177710342', '6593886300034.jpeg'),
+(14, 'nindia marselly', 'ibru', '19', 'mahasiswa', 'marselynindia@gmail.com', 'karyawan', '082177710342', '65938885bc95f.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
